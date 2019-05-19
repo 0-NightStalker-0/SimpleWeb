fa6010262dd7929cf8e57e332d8605861b54dceb
Night Stalker <skrepak@gmail.com>
2019-05-19 18:23:45 +1000

    Task 2: Implement a way of getting the height for a person based on the following logic: For Angles and Saxons, Height = (1.5 + (Age * 0.45)) For Jutes, Height = ((age * 1.6) / 2) For Hawaiian, Height = (1.7 + ((age + 2) * 0.23))

---
+++ SimpleWeb/Models/PersonExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace SimpleWeb.Models
+{
+    public static class PersonExtensions
+    {
+        public static double Height(this Person person)
+        {
+            switch (person.Race)
+            {
+                case Race.Angles:
+                case Race.Saxons:
+                    return 1.5 + (person.Age * 0.45);
+
+                case Race.Jutes:
+                    return person.Age * 1.6 / 2;
+
+                case Race.Hawaiian:
+                    return 1.7 + ((person.Age + 2) * 0.23);
+
+                default:
+                    return 0;
+            }
+        }
+    }
+}
