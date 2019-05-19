1317a6488da1760adcd2365eebf420498c1cdf2a
Night Stalker <skrepak@gmail.com>
2019-05-19 18:07:08 +1000

    Initial load

---
+++ SimpleWeb/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SimpleWeb.Controllers
+{
+    using Models;
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public static List<Person> InitPeople()
+        {
+            List<Person> people = new List<Person>();
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                people.Add(new Person()
+                {
+                    Name = "Person #" + i.ToString(),
+                    Age = rnd.Next(1, 99)
+                });
+            }
+
+            return people;
+        }
+    }
+}
---
+++ SimpleWeb/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SimpleWeb.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return "My name is '" + Name + "' and I am " + Age + " years old.";
+        }
+    }
+}
